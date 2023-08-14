445a0004b73d7df39bfacbf7954fb331a723260b
VallyProg <133013769+VallyProg@users.noreply.github.com>
2023-08-14 18:46:52 +0300

    Add files via upload

---
+++ Hometask4.py
@@ -0,0 +1,24 @@
+# import NumPy as np
+
+# arr_matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# arr_transpose = arr_matrix.transpose()
+# print (arr_matrix)
+
+# print (arr_transpose)
+
+''' Почему то не хочет запускаться в вижл студио, а в онлайн компиляторе запускается '''
+
+
+
+# def kwargs_to_dict(**kwargs):
+#     result = {}
+#     for key, value in kwargs.items():
+#         try:
+#             result[value] = key
+#         except:
+#             result[str(value)] = key
+#     return result
+
+# print(kwargs_to_dict(name='Алексей', sername='Нестеров', weight=35.5,
+#                      months=['January', 'February', 'March'],
+#                      courses={'python': 'ver 3.11', 'c#': 'ver 2.5'}))
